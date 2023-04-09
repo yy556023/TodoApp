44e850a079fae0dff10dc71ea2887b8077f195a1
Yuu <yy556023@gmail.com>
2023-04-09 21:32:54 +0800

    Angular Complete

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,19 +1,40 @@
-import { AuthService } from '@abp/ng.core';
-import { Component } from '@angular/core';
+import { ToasterService } from "@abp/ng.theme.shared";
+import { Component, OnInit } from '@angular/core';
+import { TodoItemDto } from "@proxy/services/dtos";
+import { TodoService } from "@proxy/services";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-  get hasLoggedIn(): boolean {
-    return this.authService.isAuthenticated;
+
+export class HomeComponent implements OnInit {
+
+  todoItems: TodoItemDto[];
+  newTodoText: string;
+
+  constructor(
+    private todoService: TodoService,
+    private toasterService: ToasterService) { }
+
+  ngOnInit(): void {
+    this.todoService.getList().subscribe(response => {
+      this.todoItems = response;
+    });
   }
 
-  constructor(private authService: AuthService) {}
+  create(): void {
+    this.todoService.create(this.newTodoText).subscribe((result) => {
+      this.todoItems = this.todoItems.concat(result);
+      this.newTodoText = null;
+    });
+  }
 
-  login() {
-    this.authService.navigateToLogin();
+  delete(id: string): void {
+    this.todoService.delete(id).subscribe(() => {
+      this.todoItems = this.todoItems.filter(item => item.id !== id);
+      this.toasterService.info('Deleted the todo item.');
+    });
   }
 }
